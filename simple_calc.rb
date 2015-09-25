0657e5878301db995712c73e3bda00fa80b52879
Ramil Karimov <karimovr@inbox.ru>
2015-09-25 13:32:57 +0300

    0

---
+++ simple_calc.rb
@@ -0,0 +1,19 @@
+puts "Введите первое число"
+a = gets.chomp.to_i
+puts "Введите необходимую математическую операцию"
+c = gets.chomp
+puts "Введите второе число"
+b = gets.chomp.to_i
+
+if c == "+"
+	result = a + b	
+elsif c == "-"
+	result = a - b
+elsif c == "*"
+	result = a * b
+elsif c == "/"
+	result = a / b
+else
+	puts "Некорректный ввод"
+end
+puts "#{a} #{c} #{b} = #{result}" if result
